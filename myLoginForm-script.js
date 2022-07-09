8d8c6b884378a06066d6f949e0c82c5bc4061cd7
gitganesang <51353426+gitganesang@users.noreply.github.com>
2022-07-09 19:57:15 +0530

    This is a login form with AJAX.

---
+++ myLoginForm-script.js
@@ -0,0 +1,13 @@
+// console.log(document.querySelectorAll('.form'));
+// document.addEventListener('DOMContentLoaded', ready);
+
+// function ready() {
+//     console.log('DOM is ready');
+// }
+
+function resetAll() {
+    email = '';
+    pswd = '';
+    document.querySelector('#email').value = email;
+    document.querySelector('#pswd').value = pswd;
+}
